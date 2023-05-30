d8f09b230ca52f7c94a27d9054368412165242a2
jahanabir8 <abir.opediatech@gmail.com>
2023-05-30 15:34:36 +0600

    update

---
+++ assets/JS/script.js
@@ -0,0 +1,88 @@
+const cross = document.getElementById("cross");
+const comment = document.querySelector(".float__com");
+
+cross.addEventListener("click", function() {
+    comment.classList.add("cmtNone")
+})
+
+comment.addEventListener("click", function() {
+    comment.classList.add("cmtHeight")
+})
+
+// setting sound
+const setSounds = document.querySelectorAll(".setSound");
+setSounds.forEach(element => {
+  element.addEventListener("click", function() {
+    setSounds.forEach(setSound => {
+        setSound.classList.toggle("hidden");
+    });
+  });
+});
+
+const adminImg = document.querySelector(".adminImg");
+const admine = document.querySelector(".admine");
+const admin__logs = document.querySelector(".admin__logs");
+
+adminImg.addEventListener("click", function() {
+    admine.classList.toggle("admineOff")
+})
+
+const mikeElements = document.querySelectorAll(".mike");
+mikeElements.forEach(element => {
+  element.addEventListener("click", function() {
+    mikeElements.forEach(mikeElement => {
+      mikeElement.classList.toggle("hidden");
+    });
+  });
+});
+
+
+const calls = document.querySelectorAll(".call");
+calls.forEach(element => {
+  element.addEventListener("click", function() {
+    calls.forEach(call => {
+      call.classList.toggle("hidden");
+    });
+  });
+});
+
+// settings sound js
+const sounds = document.querySelectorAll(".sound");
+sounds.forEach(element => {
+  element.addEventListener("click", function() {
+    sounds.forEach(sound => {
+        sound.classList.toggle("hidden");
+    });
+  });
+});
+
+// full screen
+// https://codepen.io/Spate/pen/OXNaQL
+
+
+// sidebar js
+const sidebar = document.querySelector(".sidebar");
+const sidebarBtn = document.querySelector(".sidebarBtn");
+
+sidebarBtn.addEventListener("click", function() {
+        sidebar.classList.toggle("sideOpen");
+});
+
+
+// window click js
+window.addEventListener("click", function(event) {
+    if (!sidebarBtn.contains(event.target) && !sidebar.contains(event.target)) {
+        sidebar.classList.remove("sideOpen");
+      }
+    // if((!sidebar.contains(event.target)) ) {
+    //     sidebar.classList.remove("sideOpen");
+    // }
+
+    if ((!admine.contains(event.target))  ) {
+        admine.classList.remove("admineOff")
+      }
+
+    
+});  
+
+
